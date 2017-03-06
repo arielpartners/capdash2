747f9a3811d32d9833d18dad08dd879d02a69380
Nicholas Iorio <niorio1@gmail.com>
2017-03-06 17:02:48 -0500

    typo

--- cdserver/features/step_definitions/healthcheck_steps.rb
+++ cdserver/features/step_definitions/healthcheck_steps.rb
@@ -6,6 +6,6 @@ When(/^I navigate to the url \/healthcheck$/) do
   get '/healthcheck'
 end
 
-Then(/^I should receive the repsonse (\d+) No Content$/) do |code|
+Then(/^I should receive the response (\d+) No Content$/) do |code|
   expect(last_response.status).to eq(code.to_i)
 end
