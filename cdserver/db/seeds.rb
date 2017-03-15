0cd0c66d5ca6c25862db2cca9e92a307f045e4e9
Nicholas Iorio <niorio1@gmail.com>
2017-03-15 09:48:41 -0400

    add user seeds and use credentials in postman json

--- cdserver/db/seeds.rb
+++ cdserver/db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(email: "sample_user@dhs.nyc.gov", password: "password")
+User.create!(email: "some_person@dhs.nyc.gov", password: "swordfish")
+User.create!(email: "anotherone@dhs.nyc.gov", password: "example")
