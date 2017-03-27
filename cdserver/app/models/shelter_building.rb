622e22344c211ae36ad196563e71025e8f64c79f
Nicholas Iorio <niorio1@gmail.com>
2017-03-27 15:22:45 -0400

    add surge beds to shelterbuilding

--- cdserver/app/models/shelter_building.rb
+++ cdserver/app/models/shelter_building.rb
@@ -1,10 +1,11 @@
 # a building that houses shelter units
 class ShelterBuilding < ApplicationRecord
   has_many :floors
-  has_many :units, through: :floor
+  has_many :units, through: :floors
   has_one :address
 
-  def bed_count
-    self.units.sum(:beds)
+  def bed_count(include_surge = false)
+    beds = self.units.sum(:beds)
+    include_surge ? beds + surge_beds : beds
   end
 end
--- cdserver/db/schema.rb
+++ cdserver/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327184344) do
+ActiveRecord::Schema.define(version: 20170327190846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170327184344) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "address_id"
+    t.integer  "surge_beds"
     t.index ["address_id"], name: "index_shelter_buildings_on_address_id", using: :btree
   end
 
