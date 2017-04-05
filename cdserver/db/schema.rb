9bae137bb96126e0779d7c91db43a83d716bd4a7
Nicholas Iorio <niorio1@gmail.com>
2017-04-05 09:55:21 -0400

    add columns to units

--- cdserver/db/schema.rb
+++ cdserver/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331032241) do
+ActiveRecord::Schema.define(version: 20170404195104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,9 +75,11 @@ ActiveRecord::Schema.define(version: 20170331032241) do
     t.string   "name"
     t.integer  "beds"
     t.integer  "shelter_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.integer  "floor_id"
+    t.string   "placement_type"
+    t.string   "status"
     t.index ["floor_id"], name: "index_units_on_floor_id", using: :btree
     t.index ["shelter_id"], name: "index_units_on_shelter_id", using: :btree
   end
