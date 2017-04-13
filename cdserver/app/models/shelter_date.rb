62f11d59de18b7d6e5d107bbec0a3dbef8a5fe8f
Nicholas Iorio <niorio1@gmail.com>
2017-04-13 10:42:43 -0400

    more shelterdate improvements

--- cdserver/app/models/shelter_date.rb
+++ cdserver/app/models/shelter_date.rb
@@ -1,11 +1,17 @@
+# Shelterdate takes a real datetime, and a cutoff hour (integer representing
+# hours past midnight) and returns a "shelter date"
 class ShelterDate
   include Comparable
-  attr_reader :date, :cutoff
+  attr_reader :date, :cutoff_hour
 
-  def initialize(datetime, cutoff)
-    @cutoff = cutoff
+  def initialize(datetime, cutoff_hour)
+    @cutoff_time = cutoff_hour
     datetime = DateTime.parse(datetime) if datetime.is_a? String
-    @date = datetime.hour < cutoff ? datetime.to_date - 1.day : datetime.to_date
+    @date = if datetime.hour < cutoff_hour
+              datetime.to_date - 1.day
+            else
+              datetime.to_date
+            end
   end
 
   def to_s
--- cdserver/spec/models/shelter_date_spec.rb
+++ cdserver/spec/models/shelter_date_spec.rb
@@ -1,23 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe ShelterDate, type: :model do
-  context 'before a given hour' do
-    it '#datecutoff will return the previous date' do
+  context 'before a given cutoff hour' do
+    it 'shelter date will equal the previous date' do
       shelter_date = ShelterDate.new('3rd Feb 2017 02:05:06 AM', 3)
       expect(shelter_date.date).to eq(Date.parse('2nd Feb 2017'))
     end
   end
-  context 'after a given hour' do
-    it '#datecutoff will return ' do
+  context 'after a given cutoff hour' do
+    it 'shelter date will equal the current date' do
       shelter_date = ShelterDate.new('3rd Feb 2017 06:05:06 AM', 3)
       expect(shelter_date.date).to eq(Date.parse('3nd Feb 2017'))
     end
   end
-  it 'can be sorted' do
+  context 'at a time before cutoff hour' do
+    it 'will be equal to the previous date' do
+      day_one_after_cutoff = ShelterDate.new('2nd Feb 2017 11:15:06 AM', 3)
+      day_two_before_cutoff = ShelterDate.new('3rd Feb 2017 02:05:06 AM', 3)
+      expect(day_two_before_cutoff).to eq(day_one_after_cutoff)
+    end
+  end
+  it 'can be compared' do
     feb3 = ShelterDate.new('3rd Feb 2017 06:05:06 AM', 3)
     feb7 = ShelterDate.new('7rd Feb 2017 06:05:06 AM', 3)
     expect(feb3).to be < feb7
     expect(feb7).to be > feb3
     expect(feb3).to eq(feb3)
+  end
+  it 'will sort based on shelter date, not real datetime' do
+    a = ShelterDate.new('3rd Feb 2017 02:00:00 AM', 3)
+    b = ShelterDate.new('2nd Feb 2017 07:05:06 PM', 3)
+    c = ShelterDate.new('15th Feb 2017 011:20:00 AM', 3)
+    d = ShelterDate.new('7th Feb 2017 05:10:00 PM', 3)
+    sorted = [a, b, c, d].sort
+    expect(sorted.first).to eq(a)
+    expect(sorted.last).to eq(c)
   end
 end
