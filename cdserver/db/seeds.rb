7dadaca067de638d614efbd3d7de3361f89081af
Nicholas Iorio <niorio1@gmail.com>
2017-04-13 16:54:01 -0400

    fix seeds

--- cdserver/db/seeds.rb
+++ cdserver/db/seeds.rb
@@ -2,31 +2,23 @@ User.create!(email: "sample_user@dhs.nyc.gov", password: "password")
 User.create!(email: "some_person@dhs.nyc.gov", password: "swordfish")
 User.create!(email: "anotherone@dhs.nyc.gov", password: "example")
 
-ewms = Shelter.create!(name: "East Williamsburg Men's Shelter")
-prospect = Shelter.create!(name: 'Prospect Interfaith')
+renewal = Provider.create!(name: 'Project Renewal')
+hfth = Provider.create!(name: 'Homes for the Homelss')
+
+ewms = Shelter.create!(name: "East Williamsburg Men's Shelter", provider: renewal)
+prospect = Shelter.create!(name: 'Prospect Interfaith', provider: hfth)
 
 ew1 = ShelterBuilding.create!(name: 'EW Bldg 1', shelter: ewms)
 kelly = ShelterBuilding.create!(shelter: prospect, address: Address.new(
-  street_address1: '730 Kelly Street'))
+  street_number: '730', street_name: 'Kelly Street'))
 
 ew_first_floor = Floor.create!(shelter_building: ew1, name: '1')
 kelly_1fl = Floor.create!(shelter_building: kelly, name: '1')
 kelly_2fl = Floor.create!(shelter_building: kelly, name: '2')
 kelly_3fl = Floor.create!(shelter_building: kelly, name: '3')
 
-single_units = [
-  { name: '1A', beds: 1},
-  { name: '1B', beds: 1},
-  { name: '1C', beds: 1},
-  { name: '1D', beds: 1},
-  { name: '1E', beds: 1},
-  { name: '1F', beds: 1},
-  { name: '1G', beds: 1}
-]
-
-single_units.each do |unit|
-  Unit.create!(name:unit[:name], floor: ew_first_floor, shelter: ewms,
-               beds: unit[:beds])
+12.times do |n|
+  Bed.create!(name: n, compartment: ew_first_floor)
 end
 
 family_units = [
@@ -41,6 +33,5 @@ family_units = [
 ]
 
 family_units.each do |unit|
-  Unit.create!(name: unit[:name], floor: unit[:floor], beds: unit[:beds],
-              shelter: prospect)
+  Unit.create!(name: unit[:name], compartment: unit[:floor], bed_count: unit[:beds])
 end
