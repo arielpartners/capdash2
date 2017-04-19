2ec9fefc979915b0817ba7df648c8f2ce036e33d
Nicholas Iorio <niorio1@gmail.com>
2017-04-19 11:58:15 -0400

    new seeds

--- cdserver/db/seeds.rb
+++ cdserver/db/seeds.rb
@@ -14,30 +14,58 @@ acacia = Provider.create!(name: 'ACACIA NETWORK HOUSING INC')
 bac2 = Shelter.create!(name: 'BRONX ACACIA CLUSTER II', provider: acacia)
 a2 = Address.create!(line1: '1231 Sheridan Avenue', borough: 'Bronx', city: 'Bronx', state: 'NY', zip: '10456')
 ShelterBuilding.create!(
-  address: a2, shelter: bac2, surge_beds: 3. population_group: 'Family with Children'
+  address: a2, shelter: bac2, surge_beds: 3,
+  population_group: 'Family with Children'
 )
 
+dhs = Provider.create!(name: 'NYC Department Of Homeless Services')
+auburn = Shelter.create!(name: 'Auburn Family Residence', provider: dhs)
+a3 = Address.create!(line1: '32 Auburn Place', borough: 'Brooklyn', city: 'Brooklyn', state: 'NY', zip: '11205')
+ShelterBuilding.create!(
+  address: a3, shelter: auburn, population_group: 'Families with Children'
+)
 
-ew_first_floor = Floor.create!(shelter_building: ew1, name: '1')
-kelly_1fl = Floor.create!(shelter_building: kelly, name: '1')
-kelly_2fl = Floor.create!(shelter_building: kelly, name: '2')
-kelly_3fl = Floor.create!(shelter_building: kelly, name: '3')
+life = Shelter.create!(name: 'LIFE', provider: dhs)
+a4 = Address.create!(line1: '78 Catherine Street', borough: 'Manhattan', city: 'New York', state: 'NY', zip: '10038')
+ShelterBuilding.create!(
+  address: a4, shelter: life, population_group: 'Families with Children'
+)
 
-12.times do |n|
-  Bed.create!(name: n, compartment: ew_first_floor)
-end
+aguila = Provider.create!(name: 'AGUILA')
+bna = Shelter.create!(name: 'BRONX NEIGHBORHOOD AGUILA', provider: aguila)
+a5 = Address.create!(line1: '1101 Manor Avenue', borough: 'Bronx', city: 'Bronx', state: 'NY', zip: '10472')
+ShelterBuilding.create!(
+  address: a5, shelter: bna, population_group: 'Families with Children'
+)
+a6 = Address.create!(line1: '708 EAST 243 STREET', borough: 'Bronx', city: 'Bronx', state: 'NY', zip: '10470')
+ShelterBuilding.create!(
+  address: a6, shelter: bna, population_group: 'Families with Children'
+)
 
-family_units = [
-  { name: '1A', floor: kelly_1fl, beds: 4 },
-  { name: '1B', floor: kelly_1fl, beds: 4 },
-  { name: '1C', floor: kelly_1fl, beds: 4 },
-  { name: '2A', floor: kelly_2fl, beds: 4 },
-  { name: '2B', floor: kelly_2fl, beds: 4 },
-  { name: '2C', floor: kelly_2fl, beds: 4 },
-  { name: '3A', floor: kelly_3fl, beds: 4 },
+a7 = Address.create!(line1: '1208 WESTCHESTER AVENUE', borough: 'Bronx', city: 'Bronx', state: 'NY', zip: '10459')
+ShelterBuilding.create!(address: a7, shelter: bac2, population_group: 'Families with Children')
 
+help = Provider.create!(name: 'Help U.S.A')
+hbc = Shelter.create!(name: 'HELP - BRONX CROTONA', provider: help)
+a8 = Address.create!(line1: '785 CROTONA PARK NORTH', borough: 'Bronx', city: 'Bronx', state: 'NY', zip: '10460')
+help_building = ShelterBuilding.create!(address: a8, shelter: hbc,  population_group: 'Families with Children')
+
+
+
+help_1fl = Floor.create!(shelter_building: help_building, name: '1')
+help_2fl = Floor.create!(shelter_building: help_building, name: '2')
+help_3fl = Floor.create!(shelter_building: help_building, name: '3')
+
+units = [
+  { name: '1A', floor: help_1fl, beds: 4 },
+  { name: '1B', floor: help_1fl, beds: 4 },
+  { name: '1C', floor: help_1fl, beds: 4 },
+  { name: '2A', floor: help_2fl, beds: 4 },
+  { name: '2B', floor: help_2fl, beds: 4 },
+  { name: '2C', floor: help_2fl, beds: 4 },
+  { name: '3A', floor: help_3fl, beds: 4 },
 ]
 
-family_units.each do |unit|
+units.each do |unit|
   Unit.create!(name: unit[:name], compartment: unit[:floor], bed_count: unit[:beds])
 end
