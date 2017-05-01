2df52bf04626b49595dc6cc2de9d0e54e3dfc5f1
Nicholas Iorio <niorio1@gmail.com>
2017-05-01 13:18:45 -0400

    change floor methods to not copy shelterbuilding values

--- cdserver/app/models/floor.rb
+++ cdserver/app/models/floor.rb
@@ -5,15 +5,11 @@ class Floor < ApplicationRecord
   belongs_to :case_type
   belongs_to :program_type
 
-  before_save :ensure_case_type, :ensure_program_type
-
-  private
-
-  def ensure_case_type
-    self.case_type ||= shelter_building.case_type
+  def case_type
+    super || shelter_building.case_type
   end
 
-  def ensure_program_type
-    self.program_type ||= shelter_building.program_type
+  def program_type
+    super || shelter_building.case_type
   end
 end
