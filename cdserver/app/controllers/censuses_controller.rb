a62d26a723d2929a13fdf278f50f56dad1b4dc44
Nicholas Iorio <niorio1@gmail.com>
2017-05-10 14:43:39 -0400

    fix census endpoint to always return array

--- cdserver/app/controllers/censuses_controller.rb
+++ cdserver/app/controllers/censuses_controller.rb
@@ -6,19 +6,14 @@ class CensusesController < ApplicationController
 
   def show
     if params[:author]
-      censuses = Census.where(author: params[:author])
-      render json: censuses
+      @censuses = Census.where(author: params[:author])
     else
       as_of = params[:as_of] || Date.today
-      @census = Census.order('datetime DESC, created_at DESC').where(
+      @censuses = Census.order('datetime DESC, created_at DESC').where(
         shelter_building_id: params[:building],
         shelter_date: Date.strptime(params[:shelter_date], '%m/%d/%Y')
-      ).where('created_at <= ?', as_of).first
-      if @census
-        render json: @census
-      else
-        render status: 404
-      end
+      ).where('created_at <= ?', as_of)
     end
+    render json: @censuses
   end
 end
